60136849e23f2585f21881d2dfb7a16640f6da40
Atonej <jorymana@mail.gvsu.edu>
2018-06-11 16:27:42 +0000

    added text box

--- fb/storytelling.php
+++ fb/storytelling.php
@@ -1,4 +1,46 @@
+<html>
+<head>
+<!-- including title -->
+<title>Combined Story Telling</title>
+
+<style>
+/** including CSS sizing **/
+input[type=text] {
+	width:60%;
+	padding:12px 20px;
+	margin: 8px 0;
+	box-sizing: border-box;
+}
+
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+<script>
+$(document).ready(function(){
+    $("#post").click(function(){
+        $("p").append(" <b>Appended text</b>.");
+    });
+</script>
+</head>
+
+<body>
+
+<p> Welcome to the Story Telling Page! </p>
+<br/><br/>
+<form>
+  <label for="include"> Post your portion of the story<br/></label>
+  <input type="text" id= "include" name="include">
+  <br/>
+  <!-- <input type = submit value = "Post" /> -->
+  <button id  = "post"> Post </button>
+
+</form>
 <?php
-echo "It works";
+$receive = $_GET['include'];
+
+echo $receive;
 
 ?>
+
+
+</body>
