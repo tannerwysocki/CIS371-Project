232805739f5128ac31f2903e9cdb48d784fed6f5
huttonb <huttonb@mail.gvsu.edu>
2018-06-12 00:43:17 +0000

    updated database, added page to show databse.

--- stuDatabase.php
+++ stuDatabase.php
@@ -1,7 +1,40 @@
-<?php
-
+<?PHP
+/**if ($_POST['userID'] == "" || $_POST['message'] == ""){
+    die("You must be logged in and submit a sentence.");
+}
+*/
+//print_r($_POST);
+//making a connection
 $l=mysqli_connect("localhost:6306","student2","pass2","student2");
-$message = mysqli_escape_string($l,$_POST['message']);
-$email = mysqli_escape_string($l,$_POST['email']);
+//$message = mysqli_escape_string($l,$_POST['message']);
+//$userID = mysqli_escape_string($l,$_POST['userID']);
+$student = 'Antony';
+//putting query together
+//$query = "insert into sentence'$student' (Message, UserID, Stamp) values ('$message', '$userID', now())";
+$l=mysqli_connect("localhost:6306","student2","pass2","student2");
+$query = "select * from sentence";
+    //executing query
+$r = mysqli_query($l,$query);
+echo "<table border=1 cellpadding=10 >";
+echo "<tr><th>SentenceID</td><th>Message</th><th>UserID</th><th>Stamp</th></tr>";
+while($row=mysqli_fetch_array($r))
+{
+echo "<tr>";
+    echo "<td>";
+        echo $row[SentenceID];
+    echo "</td><td>";
+        echo $row[Message];
+    echo "</td><td>";
+        echo "$row[UserID]";
+    echo "</td><td>";
+        echo $row['Stamp'];
+    echo "</td>";
 
+echo "</tr>";
+
+
+}
+echo "</table>";
 ?>
+
+
